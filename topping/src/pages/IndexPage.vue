edea2629dce676a8085a19d070d063f632075d07
yrk9 <s243151@stn.nagaokaut.ac.jp>
2025-04-13 14:10:17 +0900

    route roulette page

--- topping/src/pages/IndexPage.vue
+++ topping/src/pages/IndexPage.vue
@@ -1,43 +1,13 @@
+<!-- filepath: /home/yorokan2/webpage/topping2/topping/src/pages/IndexPage.vue -->
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
-  </q-page>
+  <div>
+    <h1>トッピングルーレット</h1>
+    <div>ルーレットを回してトッピングを決めよう！</div>
+  </div>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
-  },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+<script lang="ts">
+export default {
+  name: 'IndexPage',
+}
 </script>
